e501211efc61412fc0cf5fcd71117dc9f6904d46
Martin S <35738176+skrixx68@users.noreply.github.com>
2021-12-06 21:26:13 +0800

    initial code

---
+++ src/anteikucafepos/AnteikuCafePOS.java
@@ -0,0 +1,15 @@
+package anteikucafepos;
+
+/**
+ *
+ * @author Martin Serdenia,Arvin Gio Mangahas,Rodley Mendez,Ralf Ponce
+ * 
+ */
+public class AnteikuCafePOS {
+
+    
+    public static void main(String[] args) {
+        System.out.println("HEHE");
+    }
+    
+}
